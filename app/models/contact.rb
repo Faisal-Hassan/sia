f14b8925a60d3bca074d7b2e8b0ec43c1b11c93d
Faisal Hassan <faisal.hassanx@gmail.com>
2016-11-19 01:29:50 +0000

    Add contact form  validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base
-
+	validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 20 }
+  validates :comments, presence: true, length: { maximum: 500 }
+  
 end
