ba5cbbe1f00555522ecbe164043a0778416a9a6f
Faisal Hassan <faisal.hassanx@gmail.com>
2016-11-20 22:51:12 +0000

    Debug Sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
   before_filter :configure_permitted_parameters, if: :devise_controller?
   
   protected
-  def configure_permitted_parameters
-  	devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-  		user_params.permit({ roles: [] }, :name, :stripe_card_token, :email, :password, :password_confirmation)
-  	end
-	end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :stripe_card_token, :email, :password, :password_confirmation) }
+    end
 end
